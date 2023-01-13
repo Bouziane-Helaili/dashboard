343fdc82ba45039fd2bb08a0173711eb8528d2ac
Bouziane-Helaili <adressebouziane@hotmail.com>
2023-01-13 11:10:27 +0100

    new route

--- src/Controller/EmployeeController.php
+++ src/Controller/EmployeeController.php
@@ -2,16 +2,37 @@
 
 namespace App\Controller;
 
+use App\Entity\Employee;
+use Symfony\Bridge\Doctrine\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/employee')]
 class EmployeeController extends AbstractController
 {
-    #[Route('/employee', name: 'app_employee')]
+    #[Route('/', name: 'app_employee')]
     public function index(): Response
     {
         return $this->render('employee/index.html.twig', [
+            'controller_name' => 'EmployeeController',
+        ]);
+    }
+
+    #[Route('/ajout', name: 'app_employee_ajout')]
+    public function addEmployee(ManagerRegistry $doctrine): Response
+    {
+        $entityManager = $doctrine->getManager();
+        $employee = new Employee();
+        $employee->setFirstName('Bouziane');
+        $employee->setLastName('Helaili');
+        $employee->setAge('46');
+
+        $entityManager->persist($employee);
+
+
+
+        return $this->render('employee/detail.html.twig', [
             'controller_name' => 'EmployeeController',
         ]);
     }
